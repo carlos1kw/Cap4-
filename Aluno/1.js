a24050e9c3dee2313a8f2df6375d33d774d11505
carlos1kw <carlos1.teste2024@gmail.com>
2024-11-29 12:13:57 -0300

    Update 1.js

---
+++ Aluno/1.js
@@ -0,0 +1,4 @@
+   Conforme o texto de estudos, uma variável na programação é caracterizado como um indicador vinculado pelo programador
+a um espaço  de memória no computador que armazena um valor. As variáveis se fazem necessárias, uma vez 
+que são fundamentais para armazenar e manipular dados em um programa, além de permitir que os programadores
+possam vincular valores a ela durante a execução do programa, podendo usá-los em vários partes do código.
