4bbfae41717d988b40cceb0d7bba324170cc5e26
carlos1kw <carlos1.teste2024@gmail.com>
2024-11-29 12:19:32 -0300

    Update 2.js

---
+++ Aluno/2.js
@@ -0,0 +1,5 @@
+   A tipagem dinâmica em JavaScript implica que o tipo de uma variável não precisa ser especificado de forma
+explícita. O tipo da variável é determinado automaticamente com base no valor atribuído a ela. 
+  Ao que se refere a diferenciação da tipagem dinâmica para a estática é que na tipagem estática o tipo
+de uma variável deve ser explícito no momento em que a mesma for definida não podendo ser alterada posteriormente.
+  
