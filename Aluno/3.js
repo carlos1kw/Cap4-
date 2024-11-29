4239457e6e58d28f1453907e994d91b76e6806d3
carlos1kw <carlos1.teste2024@gmail.com>
2024-11-29 13:35:18 -0300

    Update 3.js

---
+++ Aluno/3.js
@@ -0,0 +1,3 @@
+   A escolha de nomes descritivos para variáveis em JavaScript é importante pois irá facilitar na compreensão e
+manutenção desse código, esses nomes descritivos permite que as variações sejam entendidas de forma objetiva
+otimizando o processo caso seja necessário interpretar ou corrigir o programa.
